6e9da6f041f6cf7bf49557b6940512bf44879b60
Daniel Foth <daniel.foth@ericsson.com>
2019-10-22 17:22:46 +0100

    Updates

---
+++ precache-manifest.e39d8c2cbba661a7712049c62ead3d92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71726c959df456cc34bfdb1e3cf26bc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "80d029948baa87ac8c15",
+    "url": "/portfolio/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "fabc93d39702c992d236",
+    "url": "/portfolio/static/css/main.46dc70d0.chunk.css"
+  },
+  {
+    "revision": "80d029948baa87ac8c15",
+    "url": "/portfolio/static/js/2.ab5590da.chunk.js"
+  },
+  {
+    "revision": "fabc93d39702c992d236",
+    "url": "/portfolio/static/js/main.428d3e0d.chunk.js"
+  },
+  {
+    "revision": "d79a9daea305c021b00e",
+    "url": "/portfolio/static/js/runtime~main.9681ef8f.js"
+  }
+]);
